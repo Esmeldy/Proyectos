af674c3af69833f0aba922c332e6be59387ea516
xXEsmeldyXx <esmeldyfm@hotmail.com>
2022-07-23 13:35:27 +0200

    Terminada la primera parte

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        suma(5,6,7);
+    }
+
+    public static void suma(int a, int b, int c){
+        System.out.println("La suma es: " +(a+b+c));
     }
 }
